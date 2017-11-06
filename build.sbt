1f78deab412a466699f5da5fa7d1af51115f723e
piyushknoldus <piyush@knoldus.com>
2017-11-06 11:40:25 -0800

    updated the spark dependency

--- build.sbt
+++ build.sbt
@@ -2,15 +2,16 @@ name := "SparkWordCount"
 
 version := "1.0"
 
-scalaVersion := "2.11.0"
+scalaVersion := "2.11.11"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" % "spark-core_2.11" % "2.2.0"
-)
+  "org.apache.spark" % "spark-core_2.11" % "2.1.1")
 
+/*
 assemblyMergeStrategy in assembly := {
   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
   case x => MergeStrategy.concat
 }
+*/
 
 mainClass in assembly := Some("spark.WordCount")
--- src/main/scala/spark/WordCount.scala
+++ src/main/scala/spark/WordCount.scala
@@ -6,14 +6,13 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Created by knoldus on 3/11/17.
   */
 
-object WordCount {
-  def main(args: Array[String]) {
-    print("\nEnter input file and output file path (space seprated)")
+object WordCount extends App{
+    print("\nEnter input file and output file path (space seprated)- ")
     val values = scala.io.StdIn.readLine()
     val filesPath = values.split(" ")
     val inputFile = filesPath(0)
     val outputFile = filesPath(1)
-    val conf = new SparkConf().setAppName("wordCount").setMaster("local[2]").set("spark.executor.memory","1g");
+    val conf = new SparkConf().setAppName("wordCount").setMaster("local[2]")
 
     // Create a Scala Spark Context.
     val sc = new SparkContext(conf)
@@ -25,5 +24,4 @@ object WordCount {
     val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
     // Save the word count back out to a text file, causing evaluation.
     counts.saveAsTextFile(outputFile)
-  }
 }
